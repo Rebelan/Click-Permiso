c22c5947326345f43ca7faa03d375d2f7840c49c
Rebelan <abelcm2013@gmail.com>
2026-02-02 17:25:24 +0100

    instalación react router

--- src/Pagina.tsx
+++ src/Pagina.tsx
@@ -2,17 +2,18 @@ import { Formulario } from './Componentes/Formulario'
 import { Header } from './Componentes/Header'
 import { Navbar } from './Componentes/Navbar'
 
+
 export const Pagina = () => {
     return (
         <>
+
+       
             <div id="content">
                 <header>
                     <Header />
                     <Navbar />
                 </header>
-                <form action="">
                     <Formulario />
-                </form>
                 <footer>
 
                 </footer>
